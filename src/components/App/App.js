d0206b1c075bccc0aee6e4916adeaa966020de81
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-24 13:24:47 -0600

    Fill out shell for app component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
+import Header from '../Header/Header';
+import LoginForm from '../LoginForm/LoginForm';
 
-function App() {
+class App extends Component {
+ render() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      <main>
+        <LoginForm />
+      </main>
     </div>
-  );
+  )
+ }
+  
 }
 
 export default App
