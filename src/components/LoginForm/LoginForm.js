24e83f2bffb015bb909f51693a410a989a59f436
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-24 13:34:56 -0600

    Scaffold the login form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react'
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      email: '',
+      purpose: ''
+    }
+  }
+  
   render() {
+    const { username, email, purpose } = this.state;
+
     return (
-      <form>
-        Hello
+      <form onSubmit={this.props.handleLoginSubmit}>
+        <label htmlFor="username">Username:       
+          <input 
+          id="username"
+          onChange={this.handleInput}
+          type="text" 
+          value={username}
+        />
+        </label> 
+        <label htmlFor="email">Email:       
+          <input 
+          id="email"
+          onChange={this.handleInput}
+          type="text" 
+          value={email}
+        />
+        </label> 
+        <label htmlFor="purpose">Purpose:       
+          <input 
+          id="purpose"
+          onChange={this.handleInput}
+          type="text" 
+          value={purpose}
+        />
+        </label> 
+        <button type="submit">Log In</button>
       </form>
     )
   }
