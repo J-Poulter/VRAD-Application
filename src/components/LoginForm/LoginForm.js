16d361598608fa5e565dae411902f80b33bf4aa1
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-24 13:25:35 -0600

    Create shell for LoginForm class component

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <form>
+        Hello
+      </form>
+    )
+  }
+}
+
+export default LoginForm;
