d4b9db1de642086d819629b755bf131da8cbad1d
Chris Postma <cjrpostma@gmail.com>
2020-03-24 19:56:04 -0600

    Add render content test to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,25 +5,21 @@ import logo from './logo.png'
 const Header = () => {
   return(
     <header className="header">
-
-    <img className="logo" src={logo} alt="VRAD travel logo"/>
-
-    <nav className="nav">
-      <button
-        className="button favorite-button"
-        type="button"
-      >
-        Favorites({4})
-      </button>
-      <button
-        className="button logout-button"
-        type="button"
-      >
-        Log Out
-      </button>
-    </nav>
-
-
+      <img className="logo" src={logo} alt="VRAD travel logo"/>
+      <nav className="nav">
+        <button
+          className="button favorite-button"
+          type="button"
+        >
+          Favorites({4})
+        </button>
+        <button
+          className="button logout-button"
+          type="button"
+        >
+          Log Out
+        </button>
+      </nav>
     </header>
   )
 }
