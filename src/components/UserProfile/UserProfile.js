fa0b71210f94b48d53f5a8b9d76bfbef5cf3ce48
Chris Postma <cjrpostma@gmail.com>
2020-03-24 21:02:52 -0600

    Add UserProfile component

---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const UserProfile = ({ email, purpose, username }) => {
+  render (
+    <section className="userprofile">
+      <p>{username}</p>
+      <p>{email}</p>
+      <p>{purpose}</p>
+    </section>
+  )
+}
+
+UserProfile.propTypes = {
+  email: PropTypes.string.isRequired,
+  purpose: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+}
+
+export default UserProfile;
