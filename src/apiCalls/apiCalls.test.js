6ecad1d9e51f6e41fd9858e496a30d722ff9097a
Chris Postma <cjrpostma@gmail.com>
2020-03-24 20:53:11 -0600

    Add happy path tests for apiCalls getAreaDetails method

--- src/apiCalls/apiCalls.test.js
+++ src/apiCalls/apiCalls.test.js
@@ -1,26 +1,27 @@
 import { cleanup, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { getAreas, getAreaListings } from './apiCalls';
+import { getAreas, getAreaListings, getAreaDetails } from './apiCalls';
+
+const mockAreaResponse = {
+  areas: [
+    {
+      area: "RiNo",
+      details: '/api/v1/areas/590'
+    },
+    {
+      area: "Park Hill",
+      details: '/api/v1/areas/751'
+    }
+  ]
+}
+
 
 describe('getAreas', () => {
-  const mockResponse = {
-    areas: [
-      {
-        area: "RiNo",
-        details: '/api/v1/areas/590'
-      },
-      {
-        area: "Park Hill",
-        details: '/api/v1/areas/751'
-      }
-    ]
-  }
-
   beforeEach(() => {
     window.fetch = jest.fn().mockImplementation(() => {
       return Promise.resolve({
         ok: true,
-        json: () => Promise.resolve(mockResponse)
+        json: () => Promise.resolve(mockAreaResponse)
       })
     })
   })
@@ -33,7 +34,7 @@ describe('getAreas', () => {
   })
 
   it('should return an array of areas', () => {
-    expect(getAreas()).resolves.toEqual(mockResponse);
+    expect(getAreas()).resolves.toEqual(mockAreaResponse);
   })
 
   it('should throw an error when status is not 200', () => {
@@ -53,5 +54,83 @@ describe('getAreas', () => {
 
     expect(getAreas()).rejects.toEqual(Error('Failed to fetch'))
   })
+})
 
+describe('getAreaDetails', () => {
+  const mockAreaDetailResponse = [
+    {
+    "shortname": "RiNo",
+    "id": 590,
+    "name": "River North",
+    "location": "North of Downtown Denver",
+    "about": "RiNo is a burgeoning area with new bars, restaurants and event spaces popping up all the time. Explore this thriving area of Denver today!",
+    "region_code": 6356834,
+    "quick_search": "o5kod9f5cqo0",
+    "listings": [
+      "/api/v1/listings/3",
+      "/api/v1/listings/44",
+      "/api/v1/listings/221",
+      "/api/v1/listings/744",
+      "/api/v1/listings/90",
+      "/api/v1/listings/310"
+    ]
+  },
+  {
+    "shortname": "Park Hill",
+    id: 751,
+    name: "Park Hill",
+    location: "East of Downtown Denver",
+    about: "Park Hill features one of the best views of the downtown area and surrounding mountains. With easy access to City Park and the major highways, Park Hill also includes many unique styles of homes.",
+    region_code: 6648386,
+    quick_search: "g1m0tsxzl0o0",
+    listings: [
+      '/api/v1/listings/3921',
+    '/api/v1/listings/56',
+    '/api/v1/listings/21',
+    ]
+  }
+  ]
+
+  beforeEach(() => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve()
+      })
+    })
+  })
+
+  afterEach(cleanup);
+
+  it('should call fetch with the correct url for each area', () => {
+    getAreaDetails(mockAreaResponse);
+    expect(window.fetch).toHaveBeenCalledTimes(2);
+    expect(window.fetch).toHaveBeenCalledWith('http://localhost:3001/api/v1/areas/590');
+    expect(window.fetch).toHaveBeenCalledWith('http://localhost:3001/api/v1/areas/751');
+  })
+
+  it('should return an array of areaDetails', () => {
+    window.fetch = jest.fn()
+      .mockImplementationOnce(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockAreaDetailResponse[0])
+        })
+      })
+      .mockImplementationOnce(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockAreaDetailResponse[1])
+        })
+      })
+
+    expect(getAreaDetails(mockAreaResponse)).resolves.toMatchObject(mockAreaDetailResponse);
+  })
+
+  it('should throw an error when status is not 200', () => {
+  })
+
+  it('should reject when failing to fetch', () => {
+
+  })
 })
