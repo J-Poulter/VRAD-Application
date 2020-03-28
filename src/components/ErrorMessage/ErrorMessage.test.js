38ec25210679df6b8fbd4436f25eb9afcd6689a5
Chris Postma <cjrpostma@gmail.com>
2020-03-28 17:38:00 -0600

    Add tests for Spinner

--- src/components/ErrorMessage/ErrorMessage.test.js
+++ src/components/ErrorMessage/ErrorMessage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { cleanup, fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import ErrorMessage from './ErrorMessage';
 
@@ -15,7 +15,7 @@ function renderErrorMessage(props) {
 
 const mockError = Error('mockMessage')
 
-it('shoulder render an error if passed an error', () => {
+it('should render an error if passed an error', () => {
   const { getByText } = renderErrorMessage({
     error: mockError
   });
---
+++ src/components/Spinner/Spinner.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Spinner from './Spinner';
+
+function renderSpinner(props) {
+  const utils = render(
+    <BrowserRouter>
+      <Spinner {...props} />
+    </BrowserRouter>
+  )
+  return { ...utils }
+}
+
+const mockError = Error('mockMessage')
+
+it('should render the correct content', () => {
+  const { getByText } = renderSpinner();
+
+  expect(getByText('Loading...')).toBeInTheDocument();
+})
