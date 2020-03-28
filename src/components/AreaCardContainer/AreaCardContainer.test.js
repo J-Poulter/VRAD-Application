7ac46a0c80b5a9aefbd3c59b42442509909fd05b
Jeremy Poulter <Jpoulter@gmail.com>
2020-03-28 15:35:31 -0600

    Add beforeEach setup to AreaCardContainer test file

--- src/components/AreaCardContainer/AreaCardContainer.test.js
+++ src/components/AreaCardContainer/AreaCardContainer.test.js
@@ -4,9 +4,24 @@ import '@testing-library/jest-dom';
 import AreaCardContainer from './AreaCardContainer';
 
 describe('AreaCardContainer', () => {
+  let areaCardContainer;
+
+  beforeEach(() => {
+    areaCardContainer = render(
+      <AreaCardContainer 
+        areaDetails={}
+        error={}
+        handleViewListingsClick={}
+        isLoading={}
+      />
+    )
+  })
+  
   afterEach(cleanup)
 
   it('should render the correct content', () => {
 
   })
+
+  it('should ')
 })
