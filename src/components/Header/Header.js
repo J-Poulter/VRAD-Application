75e36fcd812528c53d84185f91bca811e0237f5c
Chris Postma <cjrpostma@gmail.com>
2020-03-27 21:43:05 -0600

    Add Link to Header title and logo to return home on click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './Header.css';
 
@@ -9,10 +9,12 @@ import logo from './logo.png'
 const Header = ({ handleLogoutClick, isAuthenticated }) => {
   return(
     <header className="header">
-      <div className="header-container">
-        <img className="logo" src={logo} alt="VRAD travel logo"/>
-        <h1 className="header-title">VRAD</h1>
-      </div>
+      <Link to="/">
+        <div className="header-container">
+          <img className="logo" src={logo} alt="VRAD travel logo"/>
+          <h1 className="header-title">VRAD</h1>
+        </div>
+      </Link>
       {isAuthenticated &&
         <nav className="nav">
           <NavLink to="/" className="nav-link" onClick={handleLogoutClick} >
